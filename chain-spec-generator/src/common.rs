2d04be54d38722ade6b692fca151c9e73b47b0fa
Svyatoslav Nikolsky <svyatonik@gmail.com>
2024-05-30 12:15:22 +0300

    (for parachains: example, no bridging traces) added Ping Kusama and Pong Polkadot chains

--- chain-spec-generator/src/common.rs
+++ chain-spec-generator/src/common.rs
@@ -20,7 +20,7 @@ use crate::{
 	system_parachains_specs::{
 		AssetHubKusamaChainSpec, AssetHubPolkadotChainSpec, BridgeHubKusamaChainSpec,
 		BridgeHubPolkadotChainSpec, CollectivesPolkadotChainSpec, EncointerKusamaChainSpec,
-		GluttonKusamaChainSpec,
+		GluttonKusamaChainSpec, PingKusamaChainSpec, PongPolkadotChainSpec,
 	},
 	ChainSpec,
 };
@@ -90,6 +90,10 @@ pub fn from_json_file(filepath: &str, supported: String) -> Result<Box<dyn Chain
 			Ok(Box::new(GluttonKusamaChainSpec::from_json_file(path)?)),
 		x if x.starts_with("encointer-kusama") =>
 			Ok(Box::new(EncointerKusamaChainSpec::from_json_file(path)?)),
+		x if x.starts_with("ping-kusama") =>
+			Ok(Box::new(PingKusamaChainSpec::from_json_file(path)?)),
+		x if x.starts_with("pong-polkadot") =>
+			Ok(Box::new(PongPolkadotChainSpec::from_json_file(path)?)),
 		_ => Err(format!("Unknown chain 'id' in json file. Only supported: {supported}'")),
 	}
 }
